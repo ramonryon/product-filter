43b79e40b0ce686619c262c3c526f95835f5e917
Ramon Ryon <ramonryon@gmail.com>
2023-11-13 15:46:51 -0300

    Reset Global CSS and Initialized the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1>Product Filter</h1>
+    <main className='max-w-6xl mx-auto mt-10 px-4'>
+      <div className='py-8 border-b border-slate-200 space-y-4'>
+        <h1 className='text-5xl font-semibold'>Collection</h1>
+        <p className='text-slate-700 max-w-3xl'>We not only help you design exceptional products, but also make it easy for you to share your designs with more like-minded people.</p>
+      </div>
+      <div className='grid grid-cols-8 py-8 gap-10'>
+
+      </div>
+    </main>
   )
 }
